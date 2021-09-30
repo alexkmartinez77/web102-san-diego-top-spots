569e24dec788f152bab33eb7a32803107b1ba662
Alex Martinez <alexkmartinez77@gmail.com>
2021-09-30 10:45:20 -0700

    Added media query to expand container to 100% on smaller viewports.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
             $('#topSpotsTable').after('<tr><td class="spotName">' + object.name + '</td>'+ 
                                           '<td class="spotDesc">' + object.description + '</td>'+ 
-                                          '<td class="spotLoc">' + '<a href="https://www.google.com/maps?q=' + object.location[0] + ',' + object.location[1] + '"' + '>' + 'Find it' + '</a></td></tr>');  
+                                          '<td class="spotLoc">' + '<a href="https://www.google.com/maps?q=' + object.location[0] + ',' + object.location[1] + '"' + '>' + 'Map it' + '</a></td></tr>');  
 
         });
     }).fail(function(){
@@ -14,20 +14,5 @@ $(document).ready(function() {
     });
 });
 
-// Initialize and add the map
-function initMap() {
-// The location of Uluru
-const uluru = { lat: -25.344, lng: 131.036 };
-// The map, centered at Uluru
-const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-});
-// The marker, positioned at Uluru
-const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-});
-}
 
 
