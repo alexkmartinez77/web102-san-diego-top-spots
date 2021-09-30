73ab97e7a36f8c5835b77c09a899aa21d6e22c2c
Alex Martinez <alexkmartinez77@gmail.com>
2021-09-29 17:27:31 -0700

    App can now display the JSON file contents in the table

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
 
     $.getJSON('./data.json', function(data){
+        var table = document.getElementById('topSpotsTable');
+      
+        data.forEach(object => {
+            var name = object.name;
+            var desc = object.description;
+            var loc = object.location;
+            $('#topSpotsTable').after('<tr><td>' + name + '</td>'+ '<td>' + desc + '</td>'+ + '<td>' + loc + '</td></tr>');
 
-        var array = data;
-        array.forEach(object => {
-            console.log(object.name);
         });
+    }).fail(function(){
+        console.log("An error has occurred.");
     });
 
 
