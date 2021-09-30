8fd92d835b7c28e7490c537a0b5de589df412c30
Alex Martinez <alexkmartinez77@gmail.com>
2021-09-30 09:03:32 -0700

    Added the Materialize CSS Framework and put the table into a conatiner.

--- main.js
+++ main.js
@@ -4,14 +4,30 @@ $(document).ready(function() {
       
         data.forEach(object => {
 
-            $('#topSpotsTable').after('<tr><td>' + object.name + '</td>'+ 
-                                          '<td>' + object.description + '</td>'+ 
-                                          '<td>' + 'https://www.google.com/maps?q=' + object.location[0] + ',' + object.location[1] + '</td></tr>');  
+            $('#topSpotsTable').after('<tr><td class="spotName">' + object.name + '</td>'+ 
+                                          '<td class="spotDesc">' + object.description + '</td>'+ 
+                                          '<td class="spotLoc">' + '<a href="https://www.google.com/maps?q=' + object.location[0] + ',' + object.location[1] + '"' + '>' + 'Find it' + '</a></td></tr>');  
 
         });
     }).fail(function(){
         console.log("An error has occurred.");
     });
 });
+
+// Initialize and add the map
+function initMap() {
+// The location of Uluru
+const uluru = { lat: -25.344, lng: 131.036 };
+// The map, centered at Uluru
+const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+});
+// The marker, positioned at Uluru
+const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+});
+}
 
 
