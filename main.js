9f8aa5d4d8b5aaa16554aabfd4f24bb5862e2b31
Alex Martinez <alexkmartinez77@gmail.com>
2021-09-29 17:00:10 -0700

    main.js is working and able to access the data array and can iterate over each object in the array.

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-// write your code here
+
+    $.getJSON('./data.json', function(data){
+
+        var array = data;
+        array.forEach(object => {
+            console.log(object.name);
+        });
+    });
+
 
 });
+
+
