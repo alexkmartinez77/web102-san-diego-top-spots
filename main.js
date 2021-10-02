7e931710c552edb307e11b35c35899683e9918ed
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-01 17:04:55 -0700

    Used ES6 template literals to clean up the forEach array method

--- main.js
+++ main.js
@@ -4,15 +4,13 @@ $(document).ready(function() {
       
         data.reverse().forEach(object => {
 
-            $('#topSpotsTable').after('<tr><td class="spotName">' + object.name + '</td>'+ 
-                                          '<td class="spotDesc">' + object.description + '</td>'+ 
-                                          '<td class="spotLoc">' + '<a href="https://www.google.com/maps?q=' + object.location[0] + ',' + object.location[1] + '"' + '>' + 'Map it' + '</a></td></tr>');  
-
+            $('#topSpotsTable').after(`<tr><td class="spotName"> ${object.name} </td>
+                                           <td class="spotDesc"> ${object.description} </td>
+                                           <td class="spotLoc"><a href="https://www.google.com/maps?q=${object.location[0]},${object.location[1]}">Map it</a></td></tr>`);
         });
     }).fail(function(){
         console.log("An error has occurred.");
     });
 });
-
 
 
