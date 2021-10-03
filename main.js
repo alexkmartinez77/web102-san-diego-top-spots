f16b2652da2f0ea19341a71db90a32bfa0c39da4
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-02 19:41:47 -0700

    Formatted index.html, main.js and styles.css.

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 $(document).ready(() => {
+  $.getJSON("./data.json", (data) => {
+    data.reverse().forEach((topSpot) => {
+      //Destructure each topSpot
+      let {
+        name,
+        description,
+        location: [long, lat],
+      } = topSpot;
 
-    $.getJSON('./data.json', data => {
-      
-        data.reverse().forEach(topSpot => {
-            //Destructure each topSpot
-            let {name, description, location:[long, lat]} = topSpot;
-
-            $('#topSpotsTable').after(`<tr><td class="spotName"> ${name} </td>
+      $("#topSpotsTable").after(`<tr><td class="spotName"> ${name} </td>
                                            <td class="spotDesc"> ${description} </td>
                                            <td class="spotLoc"><a href="https://www.google.com/maps?q=${long},${lat}">Map it</a></td></tr>`);
-        });
-    }).fail(() => {
-        console.log("An error has occurred.");
     });
+  }).fail(() => {
+    console.log("An error has occurred.");
+  });
 });
-
-
